6dd9d433b1e40c355353287e4dadeaad85872faf
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2024-06-03 13:41:09 -0700

    added the singleton pattern

---
+++ src/design-patterns/creational/singleton.ts
@@ -0,0 +1,22 @@
+class DatabaseConnection { 
+
+    private static instance: DatabaseConnection | null = null;
+
+    private constructor(){}
+
+    static get get_instance(){
+
+        if( DatabaseConnection.instance ) return DatabaseConnection.instance;
+
+        return DatabaseConnection.instance = new DatabaseConnection();
+
+    }
+
+}
+
+const connection = DatabaseConnection.get_instance;
+
+const connection2 = DatabaseConnection.get_instance;
+
+console.log( connection === connection2 )
+
