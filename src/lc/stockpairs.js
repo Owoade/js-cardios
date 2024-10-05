14f830ca1fdccb9771195fc69d391e96e2b69db1
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2024-10-05 21:50:55 +0100

    done

--- src/lc/stockpairs.js
+++ src/lc/stockpairs.js
@@ -10,4 +10,28 @@
 
 function stockPairs( stocks, target ){
 
+    const seen = new Set();
+    const pairs = new Set();
+
+    for( let stock of stocks ){
+
+        const complememt = target - stock;
+
+        if( seen.has(complememt) ){
+
+            const pair = [Math.min(stock, complememt), Math.max(stock, complememt)]
+
+            pairs.add(pair.join(','));
+
+        }
+
+        seen.add(stock);
+
+    }
+
+    return pairs.size;
+
 }
+
+
+console.log(stockPairs([1,2,3,5,4,4,7], 8))
